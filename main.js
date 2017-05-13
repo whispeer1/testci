9f169b44056a225a6d148d98b5bc71fb3091960f
whispeer <ertyu>
2017-05-13 16:57:48 +0300

    sfds

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
 var app = angular.module("app", []);
     
-    app.controller("purchaseController", function ($scope) {
+    app.controller("purchaseController", function ($scope, $http) {
     	
 
     	$scope.name = "NAMENAME";
     	$scope.text = "";
+    	$scope.result = "";
 
     	$scope.click = function(){
-    		$scope.text = "clicked";
+    		$scope.text = "(new)222";
+
+    		$http({method: 'GET', url: 'http://localhost:81/api/get'})
+    		.then(function successCallback(response) {
+    			$scope.result = response.data;
+    		}, function errorCallback(response) {
+    			console.log(response)
+    		});
     	}
 
 	});
